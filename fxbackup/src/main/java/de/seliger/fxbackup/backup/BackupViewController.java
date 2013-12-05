4e52605c5684209e21759d574dde3480a558e504
Jürgen Seliger <juergen.seliger@idos.de>
2013-12-05 12:00:07 +0100

    BackupView eingebunden

---
+++ fxbackup/src/main/java/de/seliger/fxbackup/backup/BackupViewController.java
@@ -0,0 +1,15 @@
+package de.seliger.fxbackup.backup;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class BackupViewController implements Initializable {
+
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
