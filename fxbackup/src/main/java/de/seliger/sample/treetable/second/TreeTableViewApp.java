dbbff72323f1c97464b75cbd1d33a5ccf5dba753
juergen seliger <juergen.seliger@gmail.com>
2014-08-26 19:41:29 +0200

    umorganisiert

--- fxbackup/src/main/java/de/seliger/sample/treetable/second/TreeTableViewApp.java
+++ fxbackup/src/main/java/de/seliger/sample/treetable/second/TreeTableViewApp.java
@@ -18,107 +18,110 @@ import javafx.util.Callback;
 
 public class TreeTableViewApp extends Application {
 
-    @SuppressWarnings("unchecked")
-    private TreeItem<Inventory> getData() {
-        final TreeItem<Inventory> rootItem = new TreeItem<>(new Inventory("Root", new Data("Root data"), ""));
-        final TreeItem<Inventory> child1Item = new TreeItem<>(new Inventory("Child 1", new Data("Child 1 data"), "My notes"));
-        final TreeItem<Inventory> child2Item = new TreeItem<>(new Inventory("Child 2", new Data("Child 2 data"), "Notes"));
-        TreeItem<Inventory> child3Item = new TreeItem<>(new Inventory("Child 3", new Data("Child 3 data"), "Observations"));
-        rootItem.setExpanded(true);
-        rootItem.getChildren().addAll(child1Item, child2Item);
-        child1Item.getChildren().add(child3Item);
-        return rootItem;
-    }
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public Parent createContent() {
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    public Parent createContent() {
+		final TreeTableColumn<Inventory, String> nameColumn = new TreeTableColumn<Inventory, String>("Name");
+		nameColumn.setEditable(true);
+		nameColumn.setMinWidth(130);
+		nameColumn.setCellValueFactory(new TreeItemPropertyValueFactory("name"));
 
-        final TreeTableColumn<Inventory, String> nameColumn = new TreeTableColumn<Inventory, String>("Name");
-        nameColumn.setEditable(true);
-        nameColumn.setMinWidth(130);
-        nameColumn.setCellValueFactory(new TreeItemPropertyValueFactory("name"));
+		nameColumn.setCellFactory(CheckBoxTreeTableCell.<Inventory, String> forTreeTableColumn(new MyCellFactory(), true));
 
-        nameColumn.setCellFactory(CheckBoxTreeTableCell.<Inventory, String> forTreeTableColumn(new MyCellFactory(), true));
+		final TreeTableColumn<Inventory, String> dataColumn = new TreeTableColumn<>("Data");
+		dataColumn.setEditable(false);
+		dataColumn.setMinWidth(150);
+		dataColumn.setCellValueFactory(new TreeItemPropertyValueFactory("data"));
 
-        final TreeTableColumn<Inventory, String> dataColumn = new TreeTableColumn<>("Data");
-        dataColumn.setEditable(false);
-        dataColumn.setMinWidth(150);
-        dataColumn.setCellValueFactory(new TreeItemPropertyValueFactory("data"));
+		final TreeTableColumn<Inventory, String> notesColumn = new TreeTableColumn<>("Notes (editable)");
+		notesColumn.setEditable(false);
+		notesColumn.setMinWidth(150);
+		notesColumn.setCellValueFactory(new TreeItemPropertyValueFactory("notes"));
+		notesColumn.setCellFactory(TextFieldTreeTableCell.<Inventory> forTreeTableColumn());
 
-        final TreeTableColumn<Inventory, String> notesColumn = new TreeTableColumn<>("Notes (editable)");
-        notesColumn.setEditable(false);
-        notesColumn.setMinWidth(150);
-        notesColumn.setCellValueFactory(new TreeItemPropertyValueFactory("notes"));
-        notesColumn.setCellFactory(TextFieldTreeTableCell.<Inventory> forTreeTableColumn());
+		final TreeTableView treeTableView = new TreeTableView(getData());
+		treeTableView.setEditable(true);
+		treeTableView.setPrefSize(600, 400);
+		treeTableView.getColumns().setAll(nameColumn, dataColumn, notesColumn);
 
-        final TreeTableView treeTableView = new TreeTableView(getData());
-        treeTableView.setEditable(true);
-        treeTableView.setPrefSize(600, 400);
-        treeTableView.getColumns().setAll(nameColumn, dataColumn, notesColumn);
+		return treeTableView;
+	}
 
-        return treeTableView;
-    }
+	class MyCellFactory implements Callback<Integer, ObservableValue<Boolean>> {
 
-    class MyCellFactory implements Callback<Integer, ObservableValue<Boolean>> {
+		private ObservableBooleanValue observableValue;
 
-        private ObservableBooleanValue observableValue;
+		@Override
+		public ObservableValue<Boolean> call(Integer param) {
+			return new ObservableBooleanValue() {
 
-        @Override
-        public ObservableValue<Boolean> call(Integer param) {
-            return new ObservableBooleanValue() {
+				@Override
+				public void removeListener(InvalidationListener listener) {
+					// TODO Auto-generated method stub
 
-                @Override
-                public void removeListener(InvalidationListener listener) {
-                    // TODO Auto-generated method stub
+				}
 
-                }
+				@Override
+				public void addListener(InvalidationListener listener) {
+					// TODO Auto-generated method stub
 
-                @Override
-                public void addListener(InvalidationListener listener) {
-                    // TODO Auto-generated method stub
+				}
 
-                }
+				@Override
+				public void removeListener(ChangeListener<? super Boolean> listener) {
+					// TODO Auto-generated method stub
 
-                @Override
-                public void removeListener(ChangeListener<? super Boolean> listener) {
-                    // TODO Auto-generated method stub
+				}
 
-                }
+				@Override
+				public Boolean getValue() {
+					// TODO Auto-generated method stub
+					return true;
+				}
 
-                @Override
-                public Boolean getValue() {
-                    // TODO Auto-generated method stub
-                    return true;
-                }
+				@Override
+				public void addListener(ChangeListener<? super Boolean> listener) {
+					// TODO Auto-generated method stub
 
-                @Override
-                public void addListener(ChangeListener<? super Boolean> listener) {
-                    // TODO Auto-generated method stub
+				}
 
-                }
+				@Override
+				public boolean get() {
+					// TODO Auto-generated method stub
+					return false;
+				}
+			};
+		}
 
-                @Override
-                public boolean get() {
-                    // TODO Auto-generated method stub
-                    return false;
-                }
-            };
-        }
+	}
 
-    }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("TreeTableViewApp");
+		primaryStage.setScene(new Scene(createContent()));
+		primaryStage.show();
+	}
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("TreeTableViewApp");
-        primaryStage.setScene(new Scene(createContent()));
-        primaryStage.show();
-    }
+	/**
+	 * Java main for when running without JavaFX launcher
+	 * 
+	 * @param args
+	 *            command line arguments
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-    /**
-     * Java main for when running without JavaFX launcher
-     * @param args command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
+	@SuppressWarnings("unchecked")
+	private TreeItem<Inventory> getData() {
+		final TreeItem<Inventory> rootItem = new TreeItem<>(new Inventory("Root", new Data("Root data"), ""));
+		final TreeItem<Inventory> child1Item = new TreeItem<>(new Inventory("Child 1", new Data("Child 1 data"), "My notes"));
+		final TreeItem<Inventory> child2Item = new TreeItem<>(new Inventory("Child 2", new Data("Child 2 data"), "Notes"));
+		TreeItem<Inventory> child3Item = new TreeItem<>(new Inventory("Child 3", new Data("Child 3 data"), "Observations"));
+		rootItem.setExpanded(true);
+		rootItem.getChildren().addAll(child1Item, child2Item);
+		child1Item.getChildren().add(child3Item);
+		return rootItem;
+	}
+
 }
