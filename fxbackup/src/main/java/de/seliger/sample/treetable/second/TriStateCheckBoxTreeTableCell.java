c155fa127f0cd51b1848dc0c849b35d4dc61c9ef
Juergen Seliger <juergen.seliger@idos.de>
2015-01-08 10:54:18 +0100

    Methodenrümpfe erstellt

--- fxbackup/src/main/java/de/seliger/sample/treetable/second/TriStateCheckBoxTreeTableCell.java
+++ fxbackup/src/main/java/de/seliger/sample/treetable/second/TriStateCheckBoxTreeTableCell.java
@@ -19,57 +19,55 @@ import javafx.util.StringConverter;
 
 public class TriStateCheckBoxTreeTableCell extends TreeTableCell<Inventory, String> {
 
-	private final static StringConverter<String> defaultStringConverter = new StringConverter<String>() {
-		@Override
-		public String toString(String t) {
-			return t == null ? null : t.toString();
-		}
+    private final static StringConverter<String> defaultStringConverter = new StringConverter<String>() {
+        @Override
+        public String toString(String t) {
+            return t == null ? null : t.toString();
+        }
 
-		@Override
-		public String fromString(String string) {
-			return string;
-		}
-	};
+        @Override
+        public String fromString(String string) {
+            return string;
+        }
+    };
 
-	/***************************************************************************
-	 * * Static cell factories *
-	 * 
-	 * @return *
-	 **************************************************************************/
+    /**
+     * ************************************************************************
+     * * Static cell factories *
+     *
+     * @return *
+     * ************************************************************************
+     */
 
-	public static Callback<TreeTableColumn<Inventory, String>, TreeTableCell<Inventory, String>> create() {
-		return new Callback<TreeTableColumn<Inventory, String>, TreeTableCell<Inventory, String>>() {
-			@Override
-			public TreeTableCell<Inventory, String> call(TreeTableColumn<Inventory, String> list) {
-				TriStateCheckBoxTreeTableCell treeTableCell = new TriStateCheckBoxTreeTableCell(new InventoryCallback(),
-								defaultStringConverter);
-//				treeTableCell.setUserData();
-				return treeTableCell;
-			}
-		};
-	}
+    public static Callback<TreeTableColumn<Inventory, String>, TreeTableCell<Inventory, String>> create() {
+        return list -> new TriStateCheckBoxTreeTableCell(new InventoryCallback(), defaultStringConverter);
+    }
 
-	/***************************************************************************
-	 * * Fields * *
-	 **************************************************************************/
-	private final CheckBox checkBox;
+    /**
+     * ************************************************************************
+     * * Fields * *
+     * ************************************************************************
+     */
+    private final CheckBox checkBox;
 
-	private boolean showLabel;
+    private boolean showLabel;
 
-	private ObservableObjectValue<Inventory> inventoryObjectValue;
+    private ObservableObjectValue<Inventory> inventoryObjectValue;
 
-	/***************************************************************************
-	 * * Constructors * *
-	 **************************************************************************/
+    /**
+     * ************************************************************************
+     * * Constructors * *
+     * ************************************************************************
+     */
 
-	// /**
-	// * Creates a CheckBoxTreeTableCell with a custom string converter.
-	// *
-	// * @param getSelectedProperty
-	// * A {@link Callback} that will return a {@link ObservableValue} given an index from the TreeTableColumn.
-	// * @param converter
-	// * A StringConverter that, given an object of type T, will return a String that can be used to represent the object visually.
-	// */
+    // /**
+    // * Creates a CheckBoxTreeTableCell with a custom string converter.
+    // *
+    // * @param getSelectedProperty
+    // * A {@link Callback} that will return a {@link ObservableValue} given an index from the TreeTableColumn.
+    // * @param converter
+    // * A StringConverter that, given an object of type T, will return a String that can be used to represent the object visually.
+    // */
 //	public TriStateCheckBoxTreeTableCell(final Callback<Integer, ObservableValue<Boolean>> getSelectedProperty,
 //	final StringConverter<String> converter) {
 //	this.getStyleClass().add("check-box-tree-table-cell");
@@ -80,133 +78,136 @@ public class TriStateCheckBoxTreeTableCell extends TreeTableCell<Inventory, Stri
 //	setSelectedStateCallback(getSelectedProperty);
 //	setConverter(converter);
 //	}
+    private TriStateCheckBoxTreeTableCell(final Callback<Integer, ObservableObjectValue<Inventory>> inventoryCallback,
+                                          StringConverter<String> converter) {
+        this.getStyleClass().add("check-box-tree-table-cell");
+        this.checkBox = new CheckBox();
+        this.checkBox.setAllowIndeterminate(false);
 
-	private TriStateCheckBoxTreeTableCell(final Callback<Integer, ObservableObjectValue<Inventory>> inventoryCallback,
-					StringConverter<String> converter) {
-		this.getStyleClass().add("check-box-tree-table-cell");
-		this.checkBox = new CheckBox();
-		this.checkBox.setAllowIndeterminate(false);
+        setGraphic(null);
+        setSelectedStateCallback(inventoryCallback);
+        setConverter(converter);
 
-		setGraphic(null);
-		setSelectedStateCallback(inventoryCallback);
-		setConverter(converter);
+        addEventHandler(ActionEvent.ACTION, new ActionEventHandler());
+    }
 
-		addEventHandler(ActionEvent.ACTION, new ActionEventHandler());
-	}
+    /**
+     * ************************************************************************
+     * * Properties * *
+     * ************************************************************************
+     */
 
-	/***************************************************************************
-	 * * Properties * *
-	 **************************************************************************/
+    // --- converter
+    private final ObjectProperty<StringConverter<String>> converter = new SimpleObjectProperty<StringConverter<String>>(this, "converter") {
+        @Override
+        protected void invalidated() {
+            updateShowLabel();
+        }
+    };
 
-	// --- converter
-	private final ObjectProperty<StringConverter<String>> converter = new SimpleObjectProperty<StringConverter<String>>(this, "converter") {
-		@Override
-		protected void invalidated() {
-			updateShowLabel();
-		}
-	};
+    /**
+     * The {@link StringConverter} property.
+     */
+    public final ObjectProperty<StringConverter<String>> converterProperty() {
+        return converter;
+    }
 
-	/**
-	 * The {@link StringConverter} property.
-	 */
-	public final ObjectProperty<StringConverter<String>> converterProperty() {
-		return converter;
-	}
+    /**
+     * Sets the {@link StringConverter} to be used in this cell.
+     */
+    public final void setConverter(StringConverter<String> value) {
+        converterProperty().set(value);
+    }
 
-	/**
-	 * Sets the {@link StringConverter} to be used in this cell.
-	 */
-	public final void setConverter(StringConverter<String> value) {
-		converterProperty().set(value);
-	}
+    /**
+     * Returns the {@link StringConverter} used in this cell.
+     */
+    public final StringConverter<String> getConverter() {
+        return converterProperty().get();
+    }
 
-	/**
-	 * Returns the {@link StringConverter} used in this cell.
-	 */
-	public final StringConverter<String> getConverter() {
-		return converterProperty().get();
-	}
+    // --- selected state callback property
+    private final ObjectProperty<Callback<Integer, ObservableObjectValue<Inventory>>> selectedStateCallback = new SimpleObjectProperty<>(
+            this, "selectedStateCallback");
 
-	// --- selected state callback property
-	private final ObjectProperty<Callback<Integer, ObservableObjectValue<Inventory>>> selectedStateCallback = new SimpleObjectProperty<Callback<Integer, ObservableObjectValue<Inventory>>>(
-					this, "selectedStateCallback");
+    /**
+     * Property representing the {@link Callback} that is bound to by the CheckBox shown on screen.
+     */
+    public final ObjectProperty<Callback<Integer, ObservableObjectValue<Inventory>>> selectedStateCallbackProperty() {
+        return selectedStateCallback;
+    }
 
-	/**
-	 * Property representing the {@link Callback} that is bound to by the CheckBox shown on screen.
-	 */
-	public final ObjectProperty<Callback<Integer, ObservableObjectValue<Inventory>>> selectedStateCallbackProperty() {
-		return selectedStateCallback;
-	}
+    /**
+     * Sets the {@link Callback} that is bound to by the CheckBox shown on screen.
+     */
+    public final void setSelectedStateCallback(Callback<Integer, ObservableObjectValue<Inventory>> value) {
+        selectedStateCallbackProperty().set(value);
+    }
 
-	/**
-	 * Sets the {@link Callback} that is bound to by the CheckBox shown on screen.
-	 */
-	public final void setSelectedStateCallback(Callback<Integer, ObservableObjectValue<Inventory>> value) {
-		selectedStateCallbackProperty().set(value);
-	}
+    /**
+     * Returns the {@link Callback} that is bound to by the CheckBox shown on screen.
+     */
+    public final Callback<Integer, ObservableObjectValue<Inventory>> getSelectedStateCallback() {
+        return selectedStateCallbackProperty().get();
+    }
 
-	/**
-	 * Returns the {@link Callback} that is bound to by the CheckBox shown on screen.
-	 */
-	public final Callback<Integer, ObservableObjectValue<Inventory>> getSelectedStateCallback() {
-		return selectedStateCallbackProperty().get();
-	}
+    /***************************************************************************
+     * * Public API * *
+     **************************************************************************/
 
-	/***************************************************************************
-	 * * Public API * *
-	 **************************************************************************/
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void updateItem(String item, boolean empty) {
+        super.updateItem(item, empty);
 
-	/** {@inheritDoc} */
-	@Override
-	public void updateItem(String item, boolean empty) {
-		super.updateItem(item, empty);
+        if (empty) {
+            setText(null);
+            setGraphic(null);
+        } else {
+            StringConverter<String> c = getConverter();
 
-		if (empty) {
-			setText(null);
-			setGraphic(null);
-		} else {
-			StringConverter<String> c = getConverter();
+            if (showLabel) {
+                setText(c.toString(item));
+            }
+            setGraphic(checkBox);
 
-			if (showLabel) {
-				setText(c.toString(item));
-			}
-			setGraphic(checkBox);
+            // if (inventoryObjectValue instanceof InventoryProperty) {
+            // checkBox.selectedProperty().unbindBidirectional((InventoryProperty) inventoryObjectValue);
+            // }
+            // ObservableValue<?> obsValue = getSelectedProperty();
+            // if (obsValue instanceof InventoryProperty) {
+            // inventoryObjectValue = (ObservableObjectValue<Inventory>) obsValue;
+            // checkBox.selectedProperty().bindBidirectional((InventoryProperty) inventoryObjectValue);
+            // }
 
-			// if (inventoryObjectValue instanceof InventoryProperty) {
-			// checkBox.selectedProperty().unbindBidirectional((InventoryProperty) inventoryObjectValue);
-			// }
-			// ObservableValue<?> obsValue = getSelectedProperty();
-			// if (obsValue instanceof InventoryProperty) {
-			// inventoryObjectValue = (ObservableObjectValue<Inventory>) obsValue;
-			// checkBox.selectedProperty().bindBidirectional((InventoryProperty) inventoryObjectValue);
-			// }
+            checkBox.disableProperty().bind(
+                    Bindings.not(getTreeTableView().editableProperty().and(getTableColumn().editableProperty())
+                            .and(editableProperty())));
+        }
+    }
 
-			checkBox.disableProperty().bind(
-							Bindings.not(getTreeTableView().editableProperty().and(getTableColumn().editableProperty())
-											.and(editableProperty())));
-		}
-	}
+    /**
+     * ************************************************************************
+     * * Private implementation * *
+     * ************************************************************************
+     */
 
-	/***************************************************************************
-	 * * Private implementation * *
-	 **************************************************************************/
+    private void updateShowLabel() {
+        this.showLabel = converter != null;
+        this.checkBox.setAlignment(showLabel ? Pos.CENTER_LEFT : Pos.CENTER);
+    }
 
-	private void updateShowLabel() {
-		this.showLabel = converter != null;
-		this.checkBox.setAlignment(showLabel ? Pos.CENTER_LEFT : Pos.CENTER);
-	}
+    private ObservableValue<?> getSelectedProperty() {
+        return getSelectedStateCallback() != null ? getSelectedStateCallback().call(getIndex()) : getTableColumn().getCellObservableValue(getIndex());
+    }
 
-	private ObservableValue<?> getSelectedProperty() {
-		ObservableValue<?> observableValue = getSelectedStateCallback() != null ? getSelectedStateCallback().call(getIndex()) : getTableColumn().getCellObservableValue(
-				getIndex());
-		return observableValue;
-	}
-
-	private static class ActionEventHandler implements EventHandler<ActionEvent> {
-		@Override
+    private static class ActionEventHandler implements EventHandler<ActionEvent> {
+        @Override
         public void handle(ActionEvent eventHandler) {
             TriStateCheckBoxTreeTableCell treeTableCell = (TriStateCheckBoxTreeTableCell) eventHandler.getSource();
-            System.out.println(eventHandler.getEventType()+"-Event from: " +treeTableCell.getText());
+            System.out.println(eventHandler.getEventType() + "-Event from: " + treeTableCell.getText());
             boolean isSelected = treeTableCell.checkBox.isSelected();
             System.out.println("cp-selected: " + isSelected);
 
@@ -217,27 +218,42 @@ public class TriStateCheckBoxTreeTableCell extends TreeTableCell<Inventory, Stri
             int treeItemLevel = treeTableView.getTreeItemLevel(treeItem);
             System.out.println("treeItemLevel: " + treeItemLevel);
 
-			if(isRoot(treeTableView, treeItem)) {
-				// do something
-			} else {
-				System.out.println("isLeaf: " + treeItem.isLeaf());
-				System.out.println("isExpanded: " + treeItem.isExpanded());
-				TreeItem<Inventory> parent = treeItem.getParent();
-				System.out.println("item.parent.value: " + parent.getValue());
+            if (isRoot(treeTableView, treeItem)) {
+                // do something
+            } else {
+                boolean isLeaf = treeItem.isLeaf();
+                System.out.println("isLeaf: " + isLeaf);
+                System.out.println("isExpanded: " + treeItem.isExpanded());
+                TreeItem<Inventory> parent = treeItem.getParent();
+                System.out.println("item.parent.value: " + parent.getValue());
 
-				int parentRow = treeTableView.getRow(parent);
-				System.out.println("parentRow: " + parentRow);
+                int parentRow = treeTableView.getRow(parent);
+                System.out.println("parentRow: " + parentRow);
 
-				int row = treeTableView.getRow(treeItem);
-				System.out.println("row: " + row);
+                int row = treeTableView.getRow(treeItem);
+                System.out.println("row: " + row);
 
-				int rowIndex = treeTableRow.getIndex();
-				System.out.println("rowIndex: " + rowIndex);
-			}
+                int rowIndex = treeTableRow.getIndex();
+                System.out.println("rowIndex: " + rowIndex);
+
+                if (isLeaf) {
+                    updateParentsToRoot(treeTableView, treeItem, isSelected);
+                } else {
+                    updateChildren(treeTableView, treeItem, isSelected);
+                }
+            }
         }
 
-		private boolean isRoot(TreeTableView<Inventory> treeTableView, TreeItem<Inventory> treeItem) {
-			return treeTableView.getTreeItemLevel(treeItem) == 0;
-		}
-	}
+        private void updateChildren(TreeTableView<Inventory> treeTableView, TreeItem<Inventory> treeItem, boolean isSelected) {
+
+        }
+
+        private void updateParentsToRoot(TreeTableView<Inventory> treeTableView, TreeItem<Inventory> treeItem, boolean isSelected) {
+
+        }
+
+        private boolean isRoot(TreeTableView<Inventory> treeTableView, TreeItem<Inventory> treeItem) {
+            return treeTableView.getTreeItemLevel(treeItem) == 0;
+        }
+    }
 }
